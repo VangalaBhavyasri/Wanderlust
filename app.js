c38d02707144cfcb8b0eefbbd8c9e106b426031c
VangalaBhavyasri <bhavyasrivangala0@gmail.com>
2024-10-22 22:47:33 +0530

    All Files

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"/public")));
 app.engine('ejs',ejsMate);  
-// const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl=process.env.ATLASDB_URL;
 main().then(()=>{
     console.log("Connected to the database");
